30922dea86a69635cd5d0e68844aed7355553e17
Rafael Rios <rafaelrafssk@hotmail.com>
2025-12-24 11:35:21 -0300

    Clean main branch

---
+++ SwiftUIComposable/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SwiftUIComposable
+//
+//  Created by Rafael Rios on 18/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- SwiftUIComposable/SwiftUIComposableApp.swift
+++ SwiftUIComposable/SwiftUIComposableApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIComposableApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView(navigator: Navigator())
+            ContentView()
         }
     }
 }
